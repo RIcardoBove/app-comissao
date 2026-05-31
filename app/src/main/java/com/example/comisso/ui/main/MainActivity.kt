ac98f8b92655d1dd792ff200084cf4153e41adbc
Ricardo Bove <ricardobove57@gmail.com>
2026-05-31 16:18:40 -0300

    feat: initial commission app structure

---
+++ app/src/main/java/com/example/comisso/ui/main/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.comisso.ui.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.comisso.R
+import com.example.comisso.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
